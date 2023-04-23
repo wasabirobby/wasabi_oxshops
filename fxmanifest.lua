53aab32858a448809c29179b21a38c1e598db8a8
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-04-23 00:44:41 -0700

    chore: bump version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ lua54 'yes'
 
 description 'Wasabi OX Inventory Player Owned Shops'
 author 'wasabirobby#5110'
-version '1.0.2'
+version '1.0.3'
 
 shared_scripts { '@ox_lib/init.lua', 'configuration/*.lua' }
 
