21173bbc8ce8c7e963c3db1a4c7befce416f0525
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-07-05 13:12:25 +0200

    tweak: improvements

--- bridge/esx/server.lua
+++ bridge/esx/server.lua
@@ -1,8 +1,6 @@
-if not Framework.ESX() then return end
+if not IsESX() then return end
 
-local ESX = exports['es_extended']:getSharedObject()
-
-function Framework.AddMoney(acc, price)
+function AddMoney(acc, price)
     TriggerEvent('esx_addonaccount:getSharedAccount', ('society_%s'):format(acc), function(account)
         account.addMoney(price)
     end)
--- bridge/qb/server.lua
+++ bridge/qb/server.lua
@@ -1,7 +1,5 @@
-if not Framework.QBCore() then return end
+if not IsQBCore() then return end
 
-local QBCore = exports['qb-core']:GetCoreObject()
-
-function Framework.AddMoney(acc, price)
+function AddMoney(acc, price)
     exports['qb-management']:AddMoney(acc, price)
 end
